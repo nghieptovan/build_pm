cfe3f128f3049803dc567b7a4ecf4100925189e7
Nghiep TO <nghiepto@lotte.vn>
2019-12-15 10:29:49 +0700

    buld

--- assets/js/chatbox.d.ts
+++
@@ -1,5 +0,0 @@
-
-export declare function chatboxjs(isLogged, nameUser, emailUser, organizationUser);
-export declare function showchatboxjs();
-export declare function hidechatboxjs();
-export declare function enablechatboxjs();
--- assets/js/swiper.d.ts
+++
@@ -1,2 +0,0 @@
-
-export declare function swiperjs(id);
