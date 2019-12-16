ffb56f234a63002b21c6084236e1828c1eec198b
Nghiep TO <nghiepto@lotte.vn>
2019-12-16 17:51:01 +0700

    build

---
+++ precache-manifest.7c0af606d3c0d0191920d1c9474a0a42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0849b89d03f67c876eae2097a880ec68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "349948a41e06ead0638c",
+    "url": "/static/css/2.a1deee7c.chunk.css"
+  },
+  {
+    "revision": "349948a41e06ead0638c",
+    "url": "/static/js/2.08921f0f.chunk.js"
+  },
+  {
+    "revision": "3804a4e5357e819c2e76818f3594d609",
+    "url": "/static/js/2.08921f0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "293017cf981c361285b7",
+    "url": "/static/js/main.56ce6f15.chunk.js"
+  },
+  {
+    "revision": "efb1af44359b0dce101f",
+    "url": "/static/js/runtime-main.577be461.js"
+  }
+]);
